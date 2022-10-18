3e68611e0784a96633bc7004e34692313def54d1
Jacques-James <joyebot212013@gmail.com>
2022-10-18 19:34:46 -0400

    modified app code

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		string name = "Jacques"
+
+		system.out.println(name);
 	}
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # practice-mvn-nexus-sonar-laureate
 This is to practice 
+
+Sample done by new tech
